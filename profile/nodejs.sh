a20f03b5f12df64ca3308d9477c486446fc87d1f
Cameron Crothers <cameron.crothers@communitytelco.com.au>
2015-09-29 11:56:40 +1000

    Found the provile

--- profile/nodejs.sh
+++ profile/nodejs.sh
@@ -27,8 +27,15 @@ detect_memory() {
   esac
 }
 
-export PATH="$HOME/.heroku/node/bin:$HOME/bin:$HOME/node_modules/.bin:$PATH"
+
+export PATH="$HOME/.heroku/node/bin:$HOME/bin:$HOME/node_modules/.bin:$oracle_home:$PATH"
 export NODE_HOME="$HOME/.heroku/node"
+
+export ORACLE_HOME="$oracle_home"
+export LD_LIBRARY_PATH="$oracle_home:$LD_LIBRARY_PATH"
+export TNS_ADMIN="$oracle_home/network/admin"
+export OCI_LIB_DIR="$oracle_home"
+export OCI_INC_DIR="$oracle_home/sdk/include"
 
 calculate_concurrency
 log_concurrency
